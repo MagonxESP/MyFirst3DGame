82f8707dca8e83c6275d0d1d17fada1565616a4f
MagonxESP <janma.360@gmail.com>
2019-03-13 20:58:22 +0100

    primer commit

---
+++ Assets/Manager/Timer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour {
+
+    public float maxTime = 60f; // segundos
+    private float countDown = 0f;
+
+    // Start is called before the first frame update
+    void Start() {
+        countDown = maxTime;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        countDown -= Time.deltaTime;
+
+        Debug.Log("Quedan " + countDown + " segundos para terminar");
+
+        if (countDown <= 0) {
+            Debug.Log("Has perdio!!");
+        }
+    }
+
+    private void RestartScene() {
+        Moneda.cantidadMonedas = 0;
+        SceneManager.LoadScene("MainScene");
+    }
+}
---
+++ Assets/Moneda/Moneda.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moneda : MonoBehaviour {
+
+    public static int cantidadMonedas = 0;
+
+    // Start is called before the first frame update
+    void Start() {
+        Moneda.cantidadMonedas++;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log("Algo ha colisionado con la moneda");
+
+        if (other.CompareTag("Player")) {
+            Moneda.cantidadMonedas--;
+
+            if (Moneda.cantidadMonedas == 0) {
+                Debug.Log("El juego ha terminado");
+                GameObject gameManager = GameObject.Find("GameManager");
+                Destroy(gameManager); // destruir gameManager para terminar con el temporizador
+                GameObject[] fireworks = GameObject.FindGameObjectsWithTag("Fireworks");
+
+                foreach (GameObject firework in fireworks) {
+                    firework.GetComponent<ParticleSystem>().Play();
+                }
+            }
+
+            Destroy(gameObject);
+        }
+    }
+
+}
